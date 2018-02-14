43991e5b6eb75c73abca4df029d471eb18244c51
MSI\mitchm <mmaynar1@gmail.com>
2018-02-13 21:47:25 -0600

    Get gif details from GifRepository

--- giflib/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ giflib/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,15 +1,29 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.data.GifRepository;
+import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GifController
 {
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping( value = "/" )
     public String listGifs()
     {
         return "home"; //Thymeleaf will resolve this to the html file
+    }
+
+    @RequestMapping( value = "/gif" )
+    public String gifDetails( ModelMap modelMap )
+    {
+        Gif gif = gifRepository.findByName("android-explosion");
+        modelMap.put( "gif", gif );
+        return "gif-details"; //Thymeleaf will resolve this to the html file
     }
 }
