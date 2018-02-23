97576a76ff33793ccfca7ca48286f7b2407e3b9f
MSI\mitchm <mmaynar1@gmail.com>
2018-02-22 20:18:50 -0600

    Make "add category" feature work

--- giflib-hibernate/src/main/java/com/teamtreehouse/giflib/dao/CategoryDaoImpl.java
+++ giflib-hibernate/src/main/java/com/teamtreehouse/giflib/dao/CategoryDaoImpl.java
@@ -5,6 +5,7 @@ import com.teamtreehouse.giflib.model.Category;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -34,7 +35,11 @@ public class CategoryDaoImpl implements CategoryDao
     @Override
     public void save(Category category)
     {
-
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(category);
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
