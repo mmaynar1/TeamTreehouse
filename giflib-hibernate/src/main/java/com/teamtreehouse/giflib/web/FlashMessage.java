9017df52e5dc189fcb552d5f4c27aa53428aff92
MSI\mitchm <mmaynar1@gmail.com>
2018-02-23 20:22:25 -0600

    Create FlashMessage class

---
+++ giflib-hibernate/src/main/java/com/teamtreehouse/giflib/web/FlashMessage.java
@@ -0,0 +1,28 @@
+package com.teamtreehouse.giflib.web;
+
+public class FlashMessage
+{
+    private String message;
+    private Status status;
+
+    public FlashMessage(String message, Status status)
+    {
+        this.message = message;
+        this.status = status;
+    }
+
+    public static enum Status
+    {
+        SUCCESS, FAILURE
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public Status getStatus()
+    {
+        return status;
+    }
+}
