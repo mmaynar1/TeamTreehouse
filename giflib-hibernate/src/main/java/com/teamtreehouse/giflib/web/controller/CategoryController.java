d2132cc1899e5a8ab6be16ce42b5603fa1f24ef4
MSI\mitchm <mmaynar1@gmail.com>
2018-02-23 19:53:24 -0600

    Repopulate data on redirect when validation fails

--- giflib-hibernate/src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
+++ giflib-hibernate/src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
@@ -3,8 +3,6 @@ package com.teamtreehouse.giflib.web.controller;
 import com.teamtreehouse.giflib.model.Category;
 import com.teamtreehouse.giflib.service.CategoryService;
 import com.teamtreehouse.giflib.web.Color;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,9 +10,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -45,8 +43,10 @@ public class CategoryController {
     // Form for adding a new category
     @RequestMapping("categories/add")
     public String formNewCategory(Model model) {
-        // TODO: Add model attributes needed for new form
-        model.addAttribute("category", new Category());
+        if( !model.containsAttribute("category"))
+        {
+            model.addAttribute("category", new Category());
+        }
         model.addAttribute("colors", Color.values());
         return "category/form";
     }
@@ -70,9 +70,10 @@ public class CategoryController {
 
     // Add a category
     @RequestMapping(value = "/categories", method = RequestMethod.POST)
-    public String addCategory(@Valid Category category, BindingResult result) {
+    public String addCategory( @Valid Category category, BindingResult result, RedirectAttributes redirectAttributes ) {
         if(result.hasErrors())
         {
+            redirectAttributes.addFlashAttribute("category",category);
             return "redirect:/categories/add";
         }
         categoryService.save(category);
